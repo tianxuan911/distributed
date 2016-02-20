99f63819ddda60048421b68357dd3f96522679f7
田旋 <tianxuan@sandwish.cn>
2016-02-20 11:36:06 +0800

    服务提供者实现

--- src/main/java/com/tx/service/DubboDemoServiceImpl.java
+++ src/main/java/com/tx/service/DubboDemoServiceImpl.java
@@ -8,6 +8,6 @@ import com.tx.dubbo.demo.DemoService;
 public class DubboDemoServiceImpl implements DemoService {
     @Override
     public String sayHello(String s) {
-        return "你好,"+s;
+        return "你好,"+s+",我是服务提供商2";
     }
 }
